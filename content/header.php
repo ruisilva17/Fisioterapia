928f2fa1d137ea60aa01be7317a18d1678d23496
Rui <ruipedrosilva070@gmail.com>
2022-03-14 10:09:08 +0000

    Adicionei logo, sobre e navbar ajustada.

--- content/header.php
+++ content/header.php
@@ -1,17 +1,15 @@
 <header>
 <nav class="navbar navbar-expand-lg navbar-light bg-primary">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
+           <a class="navbar-brand" href="index.php"><img src="img/Logo.png" width="100" alt="Logo"></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
+             <font face="Arial" size="5px" 
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
             <a class="nav-link active" aria-current="page" href="index.php">Página Inicial</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link" href="outro.php">Fisioterapia Respiratória</a>
             </li>
             <li class="nav-item">
             <a class="nav-link" href="sobre.php">Sobre nós</a>
@@ -19,8 +17,9 @@
             <a class="nav-link" href="contato.php">Contacto</a>
             </li>
         </ul>
+     </font> 
         <form class="d-flex">
-            <button class="btn btn-outline-success" type="submit">Login</button>
+            <button class="btn btn-outline-success" href="style.css" type="submit">Login</button>
         </form>
         </div>
     </div>
--- sobre.php
+++ sobre.php
@@ -2,18 +2,16 @@
 include('content/head.php');
 include('content/header.php');
 ?>
-<main class="container p-3 rounded shadow"></main>
+<main class="container p-3 rounded shadow">
+    <center><h3>Sobre nós</h3></center>
+
+
+
+
+
+
+
+
+</main>
    
-
-
-
-
-
-
-
-
-
-
-
-
 <?php include ('content/footer.php');?>
