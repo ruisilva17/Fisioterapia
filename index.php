12dc122d40af3e956b7c01eb943e6573c15a0b05
Rui <ruipedrosilva070@gmail.com>
2022-03-21 08:59:12 +0000

    Sobre a fisioterapia respiratoria

--- index.php
+++ index.php
@@ -2,6 +2,16 @@
 include('content/head.php');
 include('content/header.php');
 ?>
-<main class="container p-3 rounded shadow">MAIN</main>
+<main class="container p-3 rounded shadow"><center><h3>Fisioterpia Respiratória</h3></center>
+<br>
+<p>
+A fisioterapia respiratória é uma fisioterapia que tenciona a prevenção e o tratamento de praticamente todas as doenças que atingem o sistema respiratório. É uma Fisioterapia que utiliza meios técnicas de avaliação e tratamento não evasivos com o objetivo de otimizar o transporte de oxigénio, deve ser sempre realizada pelo fisioterapeuta em casa, na clínica ou no hospital.
+
+No geral, consiste em exercícios que dão suporte para que o paciente respire melhor.
+
+
+
+</p>
+</main>
    
 <?php include ('content/footer.php');?>
